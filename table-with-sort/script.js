6ee210f4c775282ad8f6694025251d6263278453
justin-aquino <justin.aquino25@gmail.com>
2024-02-21 16:09:55 -0800

    adding toggle sort logic

--- table-with-sort/script.js
+++ table-with-sort/script.js
@@ -1,1 +1,27 @@
-// alert("hello world");
+let currentColumnIdx = 0;
+let sortOrder = "ascending";
+let table = document.getElementById("container");
+let rows = table.rows;
+let switching = true;
+
+const sortTable = (colIdx) => {
+ let shouldSwitch; //initialize
+ let i; //initialize
+
+ //toggle sort order
+
+ if (colIdx === currentColumnIdx) {
+  sortOrder = sortOrder === "ascending" ? "descending" : "ascending";
+ } else {
+  sortOrder = "ascending";
+  //Reset the sort indicator for other columns
+
+  let sortBtnIndicators = document.querySelectorAll("button");
+
+  sortBtnIndicators.forEach((btn) => {
+   if (btn !== btn[colIdx]) {
+    btn.classList.remove("ascending", "descending");
+   }
+  });
+ }
+};
