ee9eefc91a672f1b5dce0edda26dc20cb9ea8593
justin-aquino <justin.aquino25@gmail.com>
2024-02-21 16:50:00 -0800

    bug fix: sorting functionality now works on all columns

--- table-with-sort/script.js
+++ table-with-sort/script.js
@@ -1,12 +1,12 @@
 let currentColumnIdx = 0;
 let sortOrder = "ascending";
-let table = document.getElementById("container");
-let rows = table.rows;
-let switching = true;
 
 const sortTable = (colIdx) => {
- let shouldSwitch; //initialize
- let i; //initialize
+ let table = document.getElementById("container");
+ let rows = table.rows;
+ let shouldSwitch, i; //initialize
+ let switching = true;
+ //  let i;
 
  //toggle sort order
 
@@ -16,7 +16,7 @@ const sortTable = (colIdx) => {
   sortOrder = "ascending";
   //Reset the sort indicator for other columns
 
-  let sortBtnIndicators = document.querySelectorAll("button");
+  let sortBtnIndicators = document.querySelectorAll("#container th button");
 
   sortBtnIndicators.forEach((btn) => {
    if (btn !== btn[colIdx]) {
@@ -24,4 +24,48 @@ const sortTable = (colIdx) => {
    }
   });
  }
+
+ // Update current column index
+
+ currentColumnIdx = colIdx;
+
+ while (switching) {
+  switching = false;
+
+  for (i = 1; i < rows.length - 1; i++) {
+   shouldSwitch = false;
+   let x = rows[i].getElementsByTagName("TD")[colIdx];
+
+   let y = rows[i + 1].getElementsByTagName("TD")[colIdx];
+
+   if (sortOrder === "ascending") {
+    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+     shouldSwitch = true;
+     break;
+    }
+   } else {
+    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+     shouldSwitch = true;
+     break;
+    }
+   }
+  }
+  if (shouldSwitch) {
+   rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+   switching = true;
+  }
+ }
+
+ let orderIndicator = document.querySelectorAll(".order button")[colIdx];
+
+ orderIndicator.textContent =
+  sortOrder === "ascending" ? "descending" : "ascending";
+
+ let sortBtnIndicators = document.querySelectorAll("#container th button");
+
+ sortBtnIndicators[currentColumnIdx].classList.remove(
+  "ascending",
+  "descending"
+ );
+ sortBtnIndicators[currentColumnIdx].classList.add(sortOrder);
 };
