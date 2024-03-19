3e8026fe5f1a88d606e08e176b6f91dc023456fe
justin-aquino <justin.aquino25@gmail.com>
2024-03-19 11:32:01 -0700

    update current column index JS function

--- table-with-sort/script.js
+++ table-with-sort/script.js
@@ -1,41 +1,34 @@
-let currentColumnIdx = 0;
+let currentColumnIndex = 0;
 let sortOrder = "ascending";
 
-const sortTable = (colIdx) => {
+function sortTable(colIdx) {
  let table = document.getElementById("container");
  let rows = table.rows;
- let shouldSwitch, i; //initialize
  let switching = true;
- //  let i;
+ let shouldSwitch, i;
 
- //toggle sort order
+ //Toggle sort order
 
- if (colIdx === currentColumnIdx) {
+ if (colIdx === currentColumnIndex) {
   sortOrder = sortOrder === "ascending" ? "descending" : "ascending";
  } else {
   sortOrder = "ascending";
-  //Reset the sort indicator for other columns
-
-  let sortBtnIndicators = document.querySelectorAll("#container th button");
-
-  sortBtnIndicators.forEach((btn) => {
-   if (btn !== btn[colIdx]) {
-    btn.classList.remove("ascending", "descending");
+  //reset order indicator for other columns
+  let headers = document.querySelectorAll("#container th button");
+  headers.forEach((header) => {
+   if (header !== headers[colIdx]) {
+    header.classList.remove("ascending", "descending");
    }
   });
  }
 
- // Update current column index
-
- currentColumnIdx = colIdx;
+ //update current column index
 
  while (switching) {
   switching = false;
-
   for (i = 1; i < rows.length - 1; i++) {
    shouldSwitch = false;
    let x = rows[i].getElementsByTagName("TD")[colIdx];
-
    let y = rows[i + 1].getElementsByTagName("TD")[colIdx];
 
    if (sortOrder === "ascending") {
@@ -50,22 +43,10 @@ const sortTable = (colIdx) => {
     }
    }
   }
+
   if (shouldSwitch) {
    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
    switching = true;
   }
  }
-
- let orderIndicator = document.querySelectorAll(".order button")[colIdx];
-
- orderIndicator.textContent =
-  sortOrder === "ascending" ? "descending" : "ascending";
-
- let sortBtnIndicators = document.querySelectorAll("#container th button");
-
- sortBtnIndicators[currentColumnIdx].classList.remove(
-  "ascending",
-  "descending"
- );
- sortBtnIndicators[currentColumnIdx].classList.add(sortOrder);
-};
+}
