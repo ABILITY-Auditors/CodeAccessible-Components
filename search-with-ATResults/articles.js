02ef242778b47598f696f8be88ff387a0cc1d572
Justin Aquino <justin.aquino25@gmail.com>
2024-07-11 14:49:47 -0700

    fixing display function. Doesn't work yet.

---
+++ search-with-ATResults/articles.js
@@ -0,0 +1,37 @@
+export const articles = [
+    {
+    title: "Article 1",
+    author: "Seth Chan",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quaerat officia ipsa. Fugit at vero libero incidunt ipsam iusto reiciendis velsequi consequuntur, minima facere placeat voluptates debitis dignissimosdoloribus
+    },
+    {
+    title: "Article 2",
+    author: "Sean Chan",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quaerat officia ipsa. Fugit at vero libero incidunt ipsam iusto reiciendis velsequi consequuntur, minima facere placeat voluptates debitis dignissimosdoloribus
+    },
+    {
+        title: "Article 3",
+        author: "Yohan",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quaerat officia ipsa. Fugit at vero libero incidunt ipsam iusto reiciendis velsequi consequuntur, minima facere placeat voluptates debitis dignissimosdoloribus
+    },
+    {
+        title: "Article 4",
+        author: "Tippy Aquino",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quaerat officia ipsa. Fugit at vero libero incidunt ipsam iusto reiciendis velsequi consequuntur, minima facere placeat voluptates debitis dignissimosdoloribus
+    },
+    {
+        title: "Article 5",
+        author: "Arya Miller",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quaerat officia ipsa. Fugit at vero libero incidunt ipsam iusto reiciendis velsequi consequuntur, minima facere placeat voluptates debitis dignissimosdoloribus
+    },
+    {
+        title: "Article 6",
+        author: "Abby Miller",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quaerat officia ipsa. Fugit at vero libero incidunt ipsam iusto reiciendis velsequi consequuntur, minima facere placeat voluptates debitis dignissimosdoloribus
+    },
+    {
+        title: "Article 7",
+        author: "Jeff Aquino",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quaerat officia ipsa. Fugit at vero libero incidunt ipsam iusto reiciendis velsequi consequuntur, minima facere placeat voluptates debitis dignissimosdoloribus
+    },
+]
