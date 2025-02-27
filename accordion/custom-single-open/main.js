d1ad689dc69b3871edc310f0471746bce39cf5fe
Colin Bradford <cjthehippo@gmail.com>
2025-02-27 14:42:55 -0800

    renamed folders, updated custom single open css

--- accordion/custom-single-open/main.js
+++ accordion/custom-single-open/main.js
@@ -1,6 +1,6 @@
 
 // find all accordion groups
-let accordionGroup = document.getElementById('single-open-accordion-group');
+let accordionGroup = document.getElementById('cso-accordion-group');
 // find all accordion controllers in the example group
 let accordionControllers = accordionGroup.querySelectorAll(".accordion-controller")
 // add event listener to each controller
@@ -25,5 +25,7 @@ function switchAccordion(event) {
     // If the content is only visually hidden while collapsed (e.g.
     // opacity is set to 0, or height is set to 0, typically done
     // to ensure an animation works properly) then the content will
-    // need to be hidden using ARIA-HIDDEN attribute
+    // need to be hidden using ARIA-HIDDEN attribute and any focusable
+    // elements will need to be removed from focus order (can be done
+    // by adding TABINDEX=-1)
 }
